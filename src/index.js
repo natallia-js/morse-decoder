45d132530d932e7455ab1ed409790ea8c5f736de
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-09-23 11:41:04 +0300

    feat: implement a decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+var encodings = new Map();
+
+function makeNewEncodings() {
+  for (let key in MORSE_TABLE) {
+    let new_key = key;
+    new_key = new_key.replace(/\./g,'10');
+    new_key = new_key.replace(/-/g,'11');
+
+    let add_zeroes_count = 10 - new_key.length;
+    for (let i = 0; i < add_zeroes_count; i++) {
+      new_key = "0" + new_key;
+    }
+
+    encodings[new_key] = MORSE_TABLE[key];
+  }
+
+  encodings["**********"] = " ";
+}
+
 function decode(expr) {
-    // write your solution here
+  if (expr.length % 10 != 0) {
+    return undefined;
+  }
+
+  makeNewEncodings();
+
+  let str_pos = 0,
+      decoded_str = "";
+
+  while (str_pos < expr.length) {
+    let substr = expr.substr(str_pos, 10);
+
+    let val = encodings[substr];
+
+    if (val === undefined) {
+      return undefined;
+    }
+
+    decoded_str += val;
+    str_pos += 10;
+  }
+
+  return decoded_str;
 }
 
 module.exports = {
